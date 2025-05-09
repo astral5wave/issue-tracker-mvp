ff6c93e96e1264716ff5e47d9f1fe53b328777b6
Amitabha Nath <supersaiyan2k03@gmail.com>
2025-05-10 00:08:44 +0530

    fix: Resolve state destructuring issue in ColumnContainer and improve KanbanBoard functionality
    - Fixed incorrect destructuring of  in .
    - Enhanced edit mode functionality for column titles.
    - Updated styles in  for better Kanban board appearance.
    - Added proper handling for draggable columns using .

--- frontend/src/components/ColumnContainer.tsx
+++ frontend/src/components/ColumnContainer.tsx
@@ -1,22 +1,89 @@
 import React from "react";
-import { Column } from "./types";
+import { Column, Id } from "./types";
+import DeleteIcon from "../icons/DeleteIcon";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 interface Props {
   column: Column;
+  deleteColumn: (id: Id) => void;
+  updateColumn: (id: Id, title: string) => void;
 }
 function ColumnContainer(props: Props) {
-  const { column } = props;
-
+  const { column, deleteColumn, updateColumn } = props;
+  const [editMode, setEditMode] = React.useState(false);
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.id,
+    data: {
+      type: "column",
+      column,
+    },
+    disabled: editMode,
+  });
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="columnBackgroundColor opacity-40 border-2 border-rose-500 w-[350px] h-[500px] max-h-[500px] rounded-md
+    flex flex-col"
+      ></div>
+    );
+  }
   return (
     <div
+      ref={setNodeRef}
+      style={style}
       className="columnBackgroundColor w-[350px] h-[500px] max-h-[500px] rounded-md
         flex flex-col"
     >
       {/* Column Title         */}
       <div
+        {...attributes}
+        {...listeners}
+        onClick={() => {
+          setEditMode(true);
+        }}
         className="mainBackgroundColor text-md h-[60px] cursor-grab rounded-md rounded-b-none
-      p-3 font-bold columnBackgroundColor border-4 "
+      p-3 font-bold columnBackgroundColor border-4 items-center flex justify-between"
       >
-        {column.title}
+        <div className=" flex gap-2">
+          <div className="flex justify-center items-center columnBackgroundColor px-2 py-1 text-sm rounded full">
+            0
+          </div>
+          {!editMode && column.title}
+          {editMode && (
+            <input
+              className="bg-black focus:border-rose-500 border-rounded outline-none px-2"
+              value={column.title}
+              onChange={(e) => updateColumn(column.id, e.target.value)}
+              autoFocus
+              onBlur={() => {
+                setEditMode(false);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") return;
+                setEditMode(false);
+              }}
+            />
+          )}
+        </div>
+        <button
+          className="stroke-gray-500 hover:stroke-white hover:columnBackgroundColor rounded px-1 py-2"
+          onClick={() => deleteColumn(column.id)}
+        >
+          <DeleteIcon />
+        </button>
       </div>
       {/* Column task Container */}
       <div className="flex flex-grow">Content</div>
--- frontend/src/components/KanbanBoard.jsx
+++ frontend/src/components/KanbanBoard.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { createPortal } from "react-dom";
 import "../App.css";
 import PlusIcon from "../icons/PlusIcon";
 import { useState } from "react";
 import ColumnContainer from "./ColumnContainer";
+import { DndContext, DragOverlay, useSensor, useSensors } from "@dnd-kit/core";
+import { arrayMove, SortableContext } from "@dnd-kit/sortable";
+import { PointerSensor } from "@dnd-kit/core";
 const KanbanBoard = () => {
   console.log("KanbanBoard rendered");
   const [columns, setColumns] = useState([]);
+  const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
   console.log(columns);
   function createNewColumn() {
     const columnToAdd = {
@@ -17,29 +22,99 @@ const KanbanBoard = () => {
   function generateId() {
     return Math.floor(Math.random() * 10001);
   }
+  function deleteColumn(id) {
+    const filteredColumns = columns.filter((col) => col.id !== id);
+    setColumns(filteredColumns);
+  }
+  function onDragStart(event) {
+    console.log("DRAG START", event);
+    if (event.active.data.current?.type === "Column") {
+      setActiveColumn(event.active.data.current.column);
+      return;
+    }
+  }
+  function onDragEnd(event) {
+    const { active, over } = event;
+    if (!over) {
+      return;
+    }
+    const activeColumnId = active.id;
+    const overColumnId = over.id;
+    if (activeColumnId === overColumnId) {
+      return;
+    }
+    setColumns((columns) => {
+      const activeColumnIndex = columns.findIndex(
+        (col) => col.id === activeColumnId
+      );
+      const overColumnIndex = columns.findIndex(
+        (col) => col.id === overColumnId
+      );
+      return arrayMove(columns, activeColumnIndex, overColumnIndex);
+    });
+  }
+  const [activeColumn, setActiveColumn] = useState(null);
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 3,
+      },
+    })
+  );
+  function updateColumn(id, title) {
+    const newColumns = columns.map((col) => {
+      if (col.id !== id) return;
+      return { ...col, title };
+    });
+    setColumns(newColumns);
+  }
   return (
     <div
       className="m-auto flex min-h-screen w-full items-center
     overflow-x-auto overflow-y-hidden px-[40px] bg-dark-background"
     >
-      <div className="m-auto flex gap-4">
-        <div className="flex gap-4">
-          {columns.map((col) => (
-            <ColumnContainer column={col} />
-          ))}
-        </div>
-        <button
-          className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg 
+      <DndContext
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+        sensors={sensors}
+      >
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+            <SortableContext items={columnsId}>
+              {columns.map((col) => (
+                <ColumnContainer
+                  key={col.id}
+                  column={col}
+                  deleteColumn={deleteColumn}
+                  updateColumn={updateColumn}
+                />
+              ))}
+            </SortableContext>
+          </div>
+          <button
+            className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg 
       bg-kanban-background border-2 columnBackgroundColor p-4 ring-rose-400 hover:ring-2
       flex gap-2 "
-          onClick={() => {
-            createNewColumn();
-          }}
-        >
-          <PlusIcon />
-          Add Column
-        </button>
-      </div>
+            onClick={() => {
+              createNewColumn();
+            }}
+          >
+            <PlusIcon />
+            Add Column
+          </button>
+        </div>
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <ColumnContainer
+                column={activeColumn}
+                deleteColumn={deleteColumn}
+              />
+            )}
+          </DragOverlay>,
+          document.body
+        )}
+      </DndContext>
     </div>
   );
 };
